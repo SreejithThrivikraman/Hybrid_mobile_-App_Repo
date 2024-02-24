8d4c2c27b43e3dac749826d01cdb62e0ef35dac8
SreejithThrivikraman <mail.sreejith.23@gmail.com>
2024-02-23 23:47:16 -0500

    Initial commit

---
+++ src/main/kotlin/functionArg.kt
@@ -0,0 +1,20 @@
+fun main() {
+    printCalculaue(6,4){ value1, value2 ->
+        value1 + value2
+    }
+
+
+    printCalculaue(6,4){ value1, value2 ->
+        value1 - value2
+    }
+
+    printCalculaue(6,4){ value1, value2 ->
+        value1 * value2
+    }
+}
+
+
+
+fun printCalculaue(value1: Int, value2: Int, calculator : (Int,Int) -> Int){
+    println("The result is : ${calculator(value1,value2)}")
+}
