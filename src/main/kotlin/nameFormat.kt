d7c0be7378f1c69aa1711f0c7e6c2309671811cc
SreejithThrivikraman <mail.sreejith.23@gmail.com>
2024-02-24 00:05:46 -0500

    Updated with Higher order function example

---
+++ src/main/kotlin/nameFormat.kt
@@ -0,0 +1,18 @@
+fun main() {
+    printFormattedName("Sreejith", "Thrivikraman") { FirstName, SecondName ->
+        SecondName + "," + FirstName
+    }
+
+    printFormattedName("Sreejith", "Thrivikraman") { FirstName, SecondName ->
+        FirstName.uppercase() + "," + SecondName.uppercase()
+    }
+
+    printFormattedName("Sreejith", "Thrivikraman") { FirstName, SecondName ->
+        FirstName.lowercase() + "," + SecondName.lowercase()
+    }
+}
+
+
+fun printFormattedName(FirstName: String, SecondName: String, formatName: (String, String) -> String) {
+    println("The formatted Name is : ${formatName(FirstName,SecondName)}")
+}
