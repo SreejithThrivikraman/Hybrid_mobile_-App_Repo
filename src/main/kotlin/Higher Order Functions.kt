a16f83d95a7f82f3b6d9f43f4c26e738340654da
SreejithThrivikraman <mail.sreejith.23@gmail.com>
2024-02-25 15:20:04 -0500

    Renamed higher order functions file.

--- src/main/kotlin/Higher Order Functions.kt
+++ src/main/kotlin/Higher Order Functions.kt
@@ -1,3 +1,4 @@
+// Example to show the use of the higher-order functions.
 fun main() {
     printCalculaue(6,4){ value1, value2 ->
         value1 + value2
@@ -16,8 +17,6 @@ fun main() {
         value1 / value2
     }
 }
-
-
 
 fun printCalculaue(value1: Int, value2: Int, calculator : (Int,Int) -> Int){
     println("The result is : ${calculator(value1,value2)}")
