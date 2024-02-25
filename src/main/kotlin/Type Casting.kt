f8e7ac8b97d8b34231847397e89ac6a8862e5668
SreejithThrivikraman <mail.sreejith.23@gmail.com>
2024-02-25 15:55:39 -0500

    Added example for type checking.

---
+++ src/main/kotlin/Type Casting.kt
@@ -0,0 +1,22 @@
+// Code example to show the type checking of a variable
+
+fun main(){
+    var testVar = 54564654646
+    var result = checkType(testVar)
+    println("The type of the test Variable is : $result")
+}
+
+fun checkType(value: Any) : String {
+        return when {
+        value is String ->  "String"
+        value is Int ->  "Integer"
+        value is Float -> "Float"
+        value is Boolean -> "Boolean"
+        value is Long -> "Long"
+        value is Char -> "Character"
+        else -> {"Invalid"}
+         }
+
+}
+
+
